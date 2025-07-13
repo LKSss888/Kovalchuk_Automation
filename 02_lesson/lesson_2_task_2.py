08f38f22c87364ed6cc05d60715fe5b899f32ffb
Elizaveta Kovalchuk <lizik-lisik@mail.ru>
2025-07-13 13:32:08 +0400

    Обновление заданий

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -1,12 +1,10 @@
 year = int(input("Введите год: "))
 
 def is_year_leap(year):
-    print(year)
+    if (year % 4 == 0):
+        return True  # Високосный
+    else:
+        return False  # Не високосный
 
-if (year % 4 == 0):
-    print('True')  # Високосный
-else:
-    print('False')   # Не високосный
 result = is_year_leap(year)
-
-print("Год ", year, " = ", {result})  # не получается вывести результаты работы функции. Выводит: Год  2011  =  {None}
+print("Год", year, "=", result)
